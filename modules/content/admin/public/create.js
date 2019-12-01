d325770d34b6841b18f6063ffff8036fedaf45a5
Nullivex <bryan@nullivex.com>
2019-12-01 00:37:52 -0700

    add missing js file

---
+++ modules/content/admin/public/create.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+ * Kado - Web Application System
+ * Copyright © 2015-2019 Bryan Tong, NULLIVEX LLC. All rights reserved.
+ * Kado <support@kado.org>
+ *
+ * This file is part of Kado and bound to the MIT license distributed within.
+ */
+
+$(document).ready(function(){
+  $('input[data-to-uri]').each(window._mirrorFieldToUri)
+})
