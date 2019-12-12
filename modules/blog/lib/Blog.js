3d8aa0476f0f2bf98c9f9dad04fd0d554b55eb2d
Nullivex <bryan@nullivex.com>
2019-12-11 19:56:36 -0700

    blog save actually returns the promise

--- modules/blog/lib/Blog.js
+++ modules/blog/lib/Blog.js
@@ -42,9 +42,10 @@ class Blog {
     let blog
     let isNewRevision = false
     let isNew = false
-    if(!data.title || !data.content)
+    if(!data.title || !data.content){
       throw new Error('Blog Title and Content are required')
-    this.get(data.id)
+    }
+    return this.get(data.id)
       .then((result) => {
         blog = result
         if(!blog){
