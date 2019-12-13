481b9dac7b7d7f1cf8f9923f64415be3ab9d0a83
Nullivex <bryan@nullivex.com>
2019-12-13 15:41:37 -0700

    complete upgrading doc module to kado 4 standards

--- modules/doc/kado.js
+++ modules/doc/kado.js
@@ -80,7 +80,8 @@ exports.search = (app,keywords,start,limit) => {
  * @param {Kado} app Main application
  */
 exports.admin = (app) => {
-  let admin = require('./admin/index')
+  const Doc = require(app.lib('Doc')).getInstance()
+  const tuiEditor = require(K.lib('tuiEditor'))
   //register permissions
   //doc permissions
   app.permission.add('/doc/create','Create Doc')
@@ -120,33 +121,248 @@ exports.admin = (app) => {
   app.nav.addItem('Doc','/doc/project/list',
     'List Projects','umbrella')
   //register routes
-  //main doc routes
   app.get('/doc',(req,res) => {
     res.redirect(301,'/doc/list')
   })
-  app.get('/doc/list',admin.list)
-  app.get('/doc/create',admin.create)
-  app.get('/doc/edit',admin.edit)
-  app.post('/doc/save',admin.save)
-  app.post('/doc/revert',admin.revert)
-  app.post('/doc/remove',admin.remove)
-  app.get('/doc/remove',admin.remove)
+  app.get('/doc/list',(req,res)=>{
+    if(!req.query.length){
+      datatableView(res)
+      res.render(
+        'doc/list',
+        {_pageTitle: app._l.doc.doc + ' ' + app._l.list}
+      )
+    } else {
+      Doc.datatable(req,res).then((result) => { res.json(result) })
+        .catch((err) => { res.json({error: err.message}) })
+    }
+  })
+  app.get('/doc/create',(req,res)=>{
+    app.asset.addScriptOnce('/js/util.js')
+    app.asset.addScriptOnce('/js/mirrorToUri.js')
+    Doc.listProjects(res.Q)
+      .then((result) => {
+        res.render('doc/create',{
+          projects: result,
+          _pageTitle: app._l.doc.doc + ' ' + app._l.create
+        })
+      })
+  })
+  app.get('/doc/edit',(req,res)=>{
+    tuiEditor(res)
+    app.asset.addScriptOnce('/js/loadTuiEditor.js')
+    app.asset.addScriptOnce('/doc/static/revertDoc.js')
+    Doc.get(req.query.id)
+      .then((result) => {
+        if(!result) throw new Error(app._l.doc.entry_not_found)
+        result.content = base64.fromByteArray(
+          Buffer.from(result.content,'utf-8')
+        )
+        res.render('doc/edit',{
+          item: result,
+          _pageTitle: app._l.edit + ' ' + app._l.doc.doc + ' ' + result.title
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.post('/doc/save',(req,res)=>{
+    Doc.save(req.body)
+      .then((result) => {
+        if(res.isJSON){
+          res.json({item: result.result.dataValues})
+        } else {
+          req.flash('success',{
+            message: app._l.doc.entry + ' ' +
+              (result.isNew ? app._l.created : app._l.saved),
+            href: app.uri.get('/doc/edit') + '?id=' + doc.id,
+            name: result.result.id
+          })
+          res.redirect(app.uri.get('/doc/list'))
+        }
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.post('/doc/revert',(req,res)=>{
+    Doc.revert(req.data)
+      .then(() => {
+        res.json({status: 'ok', message: 'Document Reverted'})
+      })
+      .catch((err) => {
+        res.status(500)
+        res.json({status: 'error', message: err.message})
+      })
+  })
+  app.post('/doc/remove',(req,res)=>{
+    if(req.query.id) req.body.remove = req.query.id.split(',')
+    if(!(req.body.remove instanceof Array)) req.body.remove = [req.body.remove]
+    Doc.remove(req.body.remove)
+      .then(() => {
+        if(res.isJSON){
+          res.json({success: app._l.doc.removed})
+        } else {
+          req.flash('success',app._l.doc.removed)
+          res.redirect('/doc/list')
+        }
+      })
+      .catch((err) => {
+        if(res.isJSON){
+          res.json({error: err.message || app._l.doc.removal_error})
+        } else {
+          res.render('error',{error: err.message})
+        }
+      })
+  })
   //project routes
   app.get('/doc/project',(req,res) => {
     res.redirect(301,'/doc/project/list')
   })
-  app.get('/doc/project/list',admin.project.list)
-  app.get('/doc/project/create',admin.project.create)
-  app.get('/doc/project/edit',admin.project.edit)
-  app.post('/doc/project/save',admin.project.save)
-  app.post('/doc/project/remove',admin.project.remove)
-  app.get('/doc/project/remove',admin.project.remove)
+  app.get('/doc/project/list',(req,res)=>{
+    if(!req.query.length){
+      datatableView(res)
+      res.render(
+        'doc/project/list',
+        {_pageTitle: app._l.doc.doc_project + ' ' + app._l.list}
+      )
+    } else {
+      Doc.datatable(req,res).then((result) => { res.json(result) })
+        .catch((err) => { res.json({error: err.message}) })
+    }
+  })
+  app.get('/doc/project/create',(req,res)=>{
+    app.asset.addScriptOnce('/js/util.js')
+    app.asset.addScriptOnce('/js/mirrorToUri.js')
+    res.render('doc/project/create',{
+      _pageTitle: app._l.doc.doc_project + ' ' + app._l.create})
+  })
+  app.get('/doc/project/edit',(req,res)=>{
+    Doc.getProject(req.query.id,res.Q)
+      .then((result) => {
+        if(!result) throw new Error(app._l.doc.entry_not_found)
+        res.render('doc/project/edit',{
+          item: result,
+          _pageTitle: app._l.doc.doc_project + ' ' +
+            app._l.create + ' ' + result.name
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.post('/doc/project/save',(req,res)=>{
+    Doc.saveProject(req.body)
+      .then((result) => {
+        if(res.isJSON){
+          res.json({item: result.result.dataValues})
+        } else {
+          req.flash('success',{
+            message: app._l.doc.entry + ' ' +
+              (result.isNew ? app._l.created : app._l.saved),
+            href: '/doc/project/edit?id=' + result.result.id,
+            name: result.result.id
+          })
+          req.flash('info','NOTE: Create a version below ' +
+            'before creating documents.')
+          res.redirect('/doc/project/edit?id=' + result.result.id)
+        }
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.post('/doc/project/remove',(req,res)=>{
+    if(req.query.id) req.body.remove = req.query.id.split(',')
+    if(!(req.body.remove instanceof Array)) req.body.remove = [req.body.remove]
+    Doc.removeProject(req.body.remove)
+      .then(() => {
+        if(res.isJSON){
+          res.json({success: app._l.doc.removed_project})
+        } else {
+          req.flash('success',app._l.doc.removed_project)
+          res.redirect('/doc/project/list')
+        }
+      })
+      .catch((err) => {
+        if(res.isJSON){
+          res.json({error: err.message || app._l.doc.removal_error})
+        } else {
+          res.render('error',{error: err.message})
+        }
+      })
+  })
   //version routes
-  app.get('/doc/version/create',admin.version.create)
-  app.get('/doc/version/edit',admin.version.edit)
-  app.post('/doc/version/save',admin.version.save)
-  app.post('/doc/version/remove',admin.version.remove)
-  app.get('/doc/version/remove',admin.version.remove)
+  app.get('/doc/version/create',(req,res)=>{
+    if(!req.query.project){
+      res.render('error',{error: 'Missing project id'})
+    } else {
+      res.render('doc/version/create',{
+        project: req.query.project,
+        _pageTitle: app._l.doc.doc_project_version + ' ' + app._l.list
+      })
+    }
+  })
+  app.get('/doc/version/edit',(req,res)=>{
+    if(req.query.DocProjectVersionId) req.query.id = req.query.DocProjectVersionId
+    Doc.getProjectVersion(req.query.id)
+      .then((result) => {
+        if(!result) throw new Error(app._l.doc.entry_not_found)
+        res.render('doc/version/edit',{
+          item: result,
+          _pageTitle: app._l.doc.doc_project_version + ' ' + app._l.list +
+            ' ' + result.name
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.post('/doc/version/save',(req,res)=>{
+    Doc.saveProjectVersion(req.body)
+      .then((result) => {
+        if(res.isJSON){
+          res.json({item: result.result.dataValues})
+        } else {
+          req.flash('success',{
+            message: app._l.doc.entry + ' ' +
+              (result.isNew ? app._l.created : app._l.saved),
+            href: '/doc/version/edit?id=' + result.id,
+            name: result.id
+          })
+          res.redirect('/doc/project/edit?id=' + result.DocProject.id)
+        }
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.post('/doc/version/remove',(req,res)=>{
+    if(req.query.id) req.body.remove = req.query.id.split(',')
+    if(req.query.project) req.body.project = req.query.project
+    if(req.query.DocProjectId) req.body.project = req.query.DocProjectId
+    if(!(req.body.remove instanceof Array)) req.body.remove = [req.body.remove]
+    Doc.removeProjectVersion(req.body.remove)
+      .then(() => {
+        if(res.isJSON){
+          res.json({success: app._l.doc.removed_version})
+        } else {
+          req.flash('success',app._l.doc.removed_version)
+          if(req.body.project){
+            res.redirect(301,'/doc/project/edit?id=' + req.body.project)
+          } else {
+            res.redirect('/doc/project/list')
+          }
+        }
+      })
+      .catch((err) => {
+        if(res.isJSON){
+          res.json({error: err.message})
+        } else {
+          res.render('error',{error: err.message})
+        }
+      })
+  })
   //static routes
   app.use('/doc/static',app.static(__dirname + '/admin/public'))
 }
@@ -157,14 +373,7 @@ exports.admin = (app) => {
  * @param {Kado} app Main application
  */
 exports.main = (app) => {
-  let main = require('./main/index')
-  //register routes
-  app.get('/doc/project',main.project.index)
-  app.get('/doc/project/:uri',main.project.entry)
-  app.get('/doc',main.index)
-  app.get('/doc/:project/:version/:uri',main.entry)
-  app.get('/doc/:project/:version',main.list)
-  app.get('/doc/:project',main.versionList)
+  const Doc = require(app.lib('Doc')).getInstance()
   //register navigation
   app.nav.addGroup('/doc','Documentation','file-alt')
   //register views
@@ -173,6 +382,102 @@ exports.main = (app) => {
   app.view.add('doc/list',__dirname + '/main/view/list.html')
   app.view.add('doc/project/entry',__dirname + '/main/view/project/entry.html')
   app.view.add('doc/project/list',__dirname + '/main/view/project/list.html')
+  //register routes
+  app.get('/doc/project',(req,res)=>{
+    Doc.listProjectsSorted(res.Q)
+      .then((results) => {
+        res.render('doc/project/list',{
+          projectList: results,
+          _pageTitle: app._l.doc.doc_project + ' ' + app._l.list
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.get('/doc/project/:uri',(req,res)=>{
+    Doc.getProject(req.query.id,res.Q)
+      .then((result) => {
+        res.render('doc/project/entry',{
+          item: result,
+          _pageTitle: result.name
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err})
+      })
+  })
+  app.get('/doc',(req,res)=>{
+    res.redirect('/doc/project')
+  })
+  app.get('/doc/:project/:version/:uri',(req,res)=>{
+    let docList
+    let q = res.Q
+    q.include = [
+      {model: DocProjectVersion, where: {name: req.params.version}, include: [
+          {model: DocProject, where: {uri: req.params.project}}
+        ]}
+    ]
+    q.order = [['sortNum','ASC']]
+    Doc.findAll(q)
+    Doc.listDocuments(req.params.version,req.params.project,res.Q)
+      .then((result) => {
+        docList = result
+        return Doc.getFull(
+          req.params.uri,
+          req.params.version,
+          req.params.project,
+          res.Q
+        )
+      })
+      .then((result) => {
+        if(!result) throw new Error('Document not found')
+        //load tui viewer
+        tuiViewer(res)
+        app.asset.addScriptOnce('/js/loadTuiViewer.js')
+        result.contentRaw = result.content
+        result.content = base64.fromByteArray(
+          Buffer.from(result.content,'utf-8'))
+        res.render('doc/entry',{
+          doc: result,
+          docList: docList,
+          _pageTitle: result.title
+        })
+      })
+      .catch((err) => {
+        if('Document not found' === err.message) res.status(404)
+        res.render('error',{error: err})
+      })
+  })
+  app.get('/doc/:project/:version',(req,res)=>{
+    Doc.listDocuments(req.params.version,req.params.project,res.Q)
+      .then((result) => {
+        if(!result) throw new Error('This project version has no documents.')
+        res.render('doc/list',{
+          project: result[0].DocProjectVersion.DocProject,
+          version: result[0].DocProjectVersion,
+          docList: result,
+          _pageTitle: app._l.doc.doc + ' ' + app._l.list
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err.message})
+      })
+  })
+  app.get('/doc/:project',(req,res)=>{
+    Doc.listProjectVersions(req.params.project,res.Q)
+      .then((result) => {
+        if(!result) throw new Error('This project has no versions')
+        res.render('doc/versionList',{
+          project: result[0].DocProject,
+          versionList: result,
+          _pageTitle: app._l.doc.doc_project_version + ' ' + app._l.list
+        })
+      })
+      .catch((err) => {
+        res.render('error',{error: err.message})
+      })
+  })
 }
 
 
---
+++ modules/doc/lib/Doc.js
@@ -0,0 +1,222 @@
+'use strict';
+/**
+ * Kado - Web Application System
+ * Copyright © 2015-2019 Bryan Tong, NULLIVEX LLC. All rights reserved.
+ * Kado <support@kado.org>
+ *
+ * This file is part of Kado and bound to the MIT license distributed within.
+ */
+const P = require('bluebird')
+const K = require('kado').getInstance()
+const crypto = require('crypto')
+const datatable = require('sequelize-datatable')
+const sequelize = K.db.sequelize
+const DocModel = sequelize.models.Doc
+const DocRevisionModel = sequelize.models.DocRevision
+const DocProjectModel = sequelize.models.DocProject
+const DocProjectVersionModel = sequelize.models.DocProjectVersion
+
+
+class Doc {
+  datatable(req,res){
+    const q = res.Q
+    q.include = [{model: DocProjectVersionModel, include: [DocProjectModel]}]
+    return datatable(DocModel,req.query,q)
+  }
+  datatableProjects(req,res){
+    return datatable(DocProjectModel,req.query,res.Q)
+  }
+  listProjectsSorted(q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.order = [['name','ASC']]
+    return DocProjectModel.findAll(q)
+  }
+  listProjects(q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.include = [DocProjectModel]
+    return DocProjectVersionModel.findAll(q)
+  }
+  listProjectVersions(uri,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.include = [{model: DocProjectModel, where: {uri: uri}}]
+    return DocProjectVersionModel.findAll(q)
+  }
+  listDocuments(version,project,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.include = [
+      {model: DocProjectVersionModel, where: {name: req.params.version},
+        include: [
+          {model: DocProjectModel, where: {uri: req.params.project}}
+        ]
+      }
+    ]
+    q.order = [['sortNum','ASC']]
+    return DocModel.findAll(q)
+  }
+  get(id,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.include = [DocRevisionModel]
+    return DocModel.findByPk(id,q)
+  }
+  getFull(uri,version,project,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.where = {uri: uri}
+    q.include = [
+      {
+        model: DocProjectVersionModel, where: {name: version},
+        include: [ {model: DocProjectModel,where: {uri: project}} ]
+      }
+    ]
+    return DocModel.findOne(q)
+  }
+  getRevision(id,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    return DocRevisionModel.findByPk(id,q)
+  }
+  getProject(id,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.include = [DocProjectVersionModel]
+    return DocProjectModel.findByPk(id,q)
+  }
+  getProjectVersion(id,q){
+    if(!q) q = K.database.queryOptions(K.config)
+    q.include = [DocProjectModel]
+    return DocProjectVersionModel.findByPk(id,q)
+  }
+  remove(list){
+    if(!(list instanceof Array)) list = [list]
+    return P.try(()=>{return list})
+      .each((id)=>{
+        return id > 0 ? DocModel.destroy({where: {id: id}}) : null
+      })
+  }
+  removeProject(list){
+    if(!(list instanceof Array)) list = [list]
+    return P.try(()=>{return list})
+      .each((id)=>{
+        return id > 0 ? DocProjectModel.destroy({where: {id: id}}) : null
+      })
+  }
+  removeProjectVersion(list){
+    if(!(list instanceof Array)) list = [list]
+    return P.try(()=>{return list})
+      .each((id)=>{
+        return id > 0 ? DocProjectVersionModel.destroy({where: {id: id}}) : null
+      })
+  }
+  save(data){
+    let isNew = false
+    let hash
+    let doc
+    let isNewRevision = false
+    return DocModel.findByPk(data.id)
+      .then((result) => {
+        if(!result) throw new Error('Document Not Found')
+        if(!data.DocProjectVersionId) throw new Error('Missing Project')
+        doc = result
+        if(!doc){
+          isNew = true
+          data.html = ''
+          data.content = ''
+          doc = DocModel.build()
+        }
+        if(data.title) doc.title = data.title
+        if(data.uri) doc.uri = data.uri
+        if(data.sortNum) doc.sortNum = data.sortNum
+        doc.DocProjectVersionId = data.DocProjectVersionId
+        //Deal with the doc revision to save the content
+        //here is how this is going to go, first we hash the content and the html
+        //if both match then we do nothing, if they don't match then we make a new
+        //revision record and then finally store the current content and html into
+        //the main doc record as the revisions only support the doc not depend on
+        //it
+        if(!data.content) data.content = ''
+        if(!data.html) data.html = ''
+        //first hash them
+        let cipher = crypto.createHash('sha256')
+        hash = cipher.update(data.html + data.content).digest('hex')
+        return DocRevisionModel.findOne({where: {hash: hash, DocId: doc.id}})
+      })
+      .then((result) => {
+        if(!result){
+          isNewRevision = true
+          let revParams = {
+            content: data.content,
+            html: data.html,
+            hash: hash,
+            DocId: doc.id
+          }
+          return DocRevisionModel.create(revParams)
+        } else {
+          return result
+        }
+      })
+      .then(() => {
+        doc.content = data.content
+        doc.html = data.html
+        return doc.save()
+      })
+  }
+  saveProject(data){
+    let isNew = false
+    this.getProject(data.id)
+      .then((result) => {
+        if(!result){
+          isNew = true
+          result = DocProjectModel.build()
+        }
+        if(data.name) result.name = data.name
+        if(data.uri) result.uri = data.uri
+        return result.save()
+      })
+      .then((result)=>{
+        return {
+          result: result,
+          isNew: isNew
+        }
+      })
+  }
+  saveProjectVersion(data){
+    let isNew = false
+    this.getProjectVersion(data.id)
+      .then((result) => {
+        if(!data.DocProjectId) throw new Error('Missing project')
+        if(!result){
+          isNew = true
+          result = DocProjectVersionModel.build({
+            DocProjectId: data.DocProjectId
+          })
+        }
+        if(data.name) result.name = data.name
+        return result.save()
+      })
+      .then((result)=> {
+        return this.getProjectVersion(result.id)
+      })
+      .then((result)=>{
+        return { result: result, isNew: isNew }
+      })
+  }
+  revert(data){
+    const that = this
+    let revision
+    this.getRevision(data.revisionId)
+      .then((result)=>{
+        if(!result) throw new Error('Revision Not Found')
+        revision = result
+        return that.get(data.docId)
+      })
+      .then((result)=>{
+        if(!result) throw new Error('Doc Not Found')
+        result.content = revision.content
+        result.html = revision.html
+        return result.save()
+      })
+  }
+}
+
+Doc.getInstance = ()=>{
+  return new Doc()
+}
+
+module.exports = Doc
